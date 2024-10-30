b5677aaa8cdade78b56172c705ed89b0ebed278c
Kacelo <vernonkachelo@gmail.com>
2024-10-30 12:59:32 +0200

    updated

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "my-protfolio-website",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
