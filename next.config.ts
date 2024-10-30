032b29b0a69e806e4a46208458f2228b475408e1
Vernon Kacelo <49986005+Kacelo@users.noreply.github.com>
2024-10-30 13:46:30 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/portfolio",
+  images: { unoptimized: true },
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
