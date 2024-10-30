730e2c41206cdefda78a9a8d3fa0507be434c7d9
Kacelo <vernonkachelo@gmail.com>
2024-10-30 12:53:01 +0200

    done with initial set up

---
+++ src/app/components/ProjectCard.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import React from "react";
+
+interface ProjectCardProps {
+  title: string;
+  description: string;
+  image: string;
+  tags: string[];
+  link: string;
+}
+
+const ProjectCard: React.FC<ProjectCardProps> = ({
+  title,
+  description,
+  image,
+  tags,
+  link
+}) => {
+  return (
+    <div className="bg-white rounded-xl shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300">
+      <Image width={500}
+              height={500} src={image} alt={title} className="w-full h-48 object-cover" />
+      <div className="p-6">
+        <h3 className="text-xl font-bold text-gray-900 mb-2">{title}</h3>
+        <p className="text-gray-600 mb-4">{description}</p>
+        <div className="flex flex-wrap gap-2">
+          {tags.map((tag, index) => (
+            <span
+              key={index}
+              className="px-3 py-1 bg-blue-100 text-blue-600 rounded-full text-sm"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+        <div className="py-6">
+          <a
+            href={link}
+            target="_blank"
+            className="px-10 py-3 bg-blue-600 text-white-600 rounded-full text-sm"
+          >
+            visit
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
